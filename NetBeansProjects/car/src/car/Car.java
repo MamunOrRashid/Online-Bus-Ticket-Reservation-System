eaa27be82c1a6b5fe72f62302fb12de0f2e234d5
MamunOrRashid <mdmamun8811@gmail.com>
2019-04-26 16:05:40 +0600

    fist commit

---
+++ NetBeansProjects/car/src/car/Car.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car;
+
+/**
+ *
+ * @author mp
+ */
+public class Car {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void check(){
+        
+    }
+    
+}
